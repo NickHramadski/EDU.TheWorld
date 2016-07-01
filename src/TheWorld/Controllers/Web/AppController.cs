11506f6d801428c6938ff296c1b4beb35bd55740
Nick Hramadski <n.hramadski@gmail.com>
2016-07-01 15:18:31 +0300

    MVC and Bootstrap parts completed

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,10 +1,18 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,5 +26,31 @@ namespace TheWorld.Controllers.Web
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Contact(ContañtViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSetting:SiteEmailAdress"];
+
+                    if (string.IsNullOrWhiteSpace(email)) 
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem.");
+                }
+
+                if (_mailService.SendMail(email,
+                     email,
+                     $"Contact Page from {model.Name} ({model.Email})",
+                     model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail sent. Thanks!";
+                }
+            }
+            return View();
+        }
     }
 }
+    
--- src/TheWorld/ViewModels/ContaсtViewModel.cs
+++ src/TheWorld/ViewModels/ContaсtViewModel.cs
@@ -7,14 +7,16 @@ using System.Threading.Tasks;
 
 namespace TheWorld.ViewModels
 {
-    public class ContactViewModel
+    public class ContañtViewModel
     {
         [Required]
         [StringLength(255, MinimumLength = 5)]
         public string Name { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         [StringLength(1024, MinimumLength = 5)]
         public string Message { get; set; }
